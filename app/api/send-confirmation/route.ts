8826de87ac6d0d7608f644d543f72cabe5092672
rafik <asia.hoceima@gmail.com>
2026-02-03 02:01:21 +0100

    add API

--- app/api/send-confirmation/route.ts
+++ app/api/send-confirmation/route.ts
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
-
+export const dynamic = 'force-dynamic';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
